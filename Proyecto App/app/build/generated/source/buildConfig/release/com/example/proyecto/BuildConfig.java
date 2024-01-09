bbe4e9ef139142c8ea7613ff2da3ce30b4807c24
Diegonmarti <diegogmarti@gmail.com>
2024-01-09 20:13:49 +0100

    Mover archivos desde la carpeta 'Perruna Pagina' a la raíz y eliminar la carpeta

---
+++ Proyecto App/app/build/generated/source/buildConfig/release/com/example/proyecto/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.proyecto;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.proyecto";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "Diegonmarti 1.0";
+}
